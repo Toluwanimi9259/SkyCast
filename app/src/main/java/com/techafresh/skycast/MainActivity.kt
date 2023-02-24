57ccbbc68695999a689bd9576d18524154e28fb9
The Mafia <issactoluwanimi@gmail.com>
2023-02-24 06:46:39 -0800

    Provided Options for non-first time users

--- app/src/main/java/com/techafresh/skycast/MainActivity.kt
+++ app/src/main/java/com/techafresh/skycast/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.techafresh.skycast
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val firstTimer : Boolean = false
+
+    lateinit var sharedPreferences : SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPreferences = this.getSharedPreferences("First_Timer_Checker" , Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isFirstTimer" , firstTimer)
+        editor.apply()
+
     }
 }
--- app/src/main/java/com/techafresh/skycast/presentation/activities/splash/SplashActivity.kt
+++ app/src/main/java/com/techafresh/skycast/presentation/activities/splash/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.techafresh.skycast.presentation.activities.splash
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -11,14 +13,27 @@ import com.techafresh.skycast.presentation.activities.onboarding.OnboardingActiv
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var sharedPreferences : SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar!!.hide()
 
+        sharedPreferences = this.getSharedPreferences("First_Timer_Checker" , Context.MODE_PRIVATE)
+
         Handler().postDelayed(Runnable {
-            startActivity(Intent(this@SplashActivity , OnboardingActivity::class.java))
+            checkFirstTimer()
             finish()
         }, 3000)
+
+    }
+
+    private fun checkFirstTimer(){
+        if (!sharedPreferences.getBoolean("isFirstTimer" , true)){
+            startActivity(Intent(this@SplashActivity , MainActivity::class.java))
+        }else{
+            startActivity(Intent(this@SplashActivity , OnboardingActivity::class.java))
+        }
     }
 }
